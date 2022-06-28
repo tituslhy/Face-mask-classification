b0668df37b075d902bb4f37fbd9c1b85ae17cf0d
teyang-lau <ty_lau@outlook.com>
2022-06-28 08:56:04 +0800

    updated streamlit

--- face_mask_classification_predict.py
+++ face_mask_classification_predict.py
@@ -5,14 +5,13 @@ from google.cloud import aiplatform as aip
 from PIL import Image
 from matplotlib import image
 import matplotlib.pyplot as plt
+import cv2
 import os
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="daring-hash-348101-9717f041dd58.json"
-PROJECT_ID = "daring-hash-348101"  # @param {type:"string"}
-REGION = "us-east1"  # @param {type: "string"}
-BUCKET_NAME = "seangoh-smu-mle-usa"
-BUCKET_URI = f"gs://{BUCKET_NAME}"
-ENDPOINT = '6005374181238112256'
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="mlops-vertexai-smu-d5d316b0b0a5.json"
+PROJECT_ID = "mlops-vertexai-smu" 
+REGION = "us-central1"  # @param {type: "string"}
+ENDPOINT = '4589827727267201024'
 
 aip.init(project=PROJECT_ID, location=REGION)
 
@@ -20,56 +19,30 @@ def load_image(image_file):
 	img = Image.open(image_file)
 	return img
 
-def predict_image(img_path, endpoint):
+def predict_image(image, endpoint):
     """Get predictions for images and plot out with the prediction
     
     Args:
-    img_path (str): path to the image
+    image (array): numpy array of image
     endpoint (str): endpoint id
     
     Returns:
     matplotlib plot with class prediction 
     """
-    
-    # convert image for plotting and sending request
-    img = np.asarray(Image.open(img_path))
-    image_data = np.resize(img, (1,200,200,3)) / 255.0
-    image_data_norm = image_data.tolist()
+
+    image_data_list = image.tolist()
     
     # get prediction
-    endpoint = aip.Endpoint(ENDPOINT)
-    prediction = endpoint.predict(instances=image_data_norm)
+    endpoint = aip.Endpoint(endpoint)
+    prediction = endpoint.predict(instances=image_data_list)
     result = np.argmax(prediction.predictions)
     result = 'With Mask' if result == 1 else 'Without Mask'
-    
-    # # plot image with prediction
-    # plt.imshow(img, interpolation='nearest')
-    # plt.suptitle('Prediction = {}'.format(result))
-    # plt.axis('off')
-    # plt.show()
+
     return result
 
-def load_image(image_file):
-	img = Image.open(image_file)
-	return img
 
 st.title('Face Mask Classification')
 
-# image_file = st.file_uploader("Choose a file", type=["png","jpg","jpeg"])
-# if image_file is not None:
-#     print(image_file)
-#      # To See details
-#     file_details = {"filename":image_file.name, "filetype":image_file.type,
-#                     "filesize":image_file.size}
-#     # st.write(file_details)
-
-#     # make prediction from endpoint
-#     result = predict_image(image_file, ENDPOINT)
-
-#     # To View Uploaded Image
-#     st.image(load_image(image_file),width=250, caption="Prediction: {}".format(result))
-
-    
 
 image_files = st.file_uploader("Choose Files", accept_multiple_files=True, type=["png","jpg","jpeg"])
 results = []
@@ -78,9 +51,15 @@ captions = []
 
 for image_file in image_files:
      # make prediction from endpoint
-    result = predict_image(image_file, ENDPOINT)
+    file_bytes = np.asarray(bytearray(image_file.read()), dtype=np.uint8)
+    opencv_image = cv2.imdecode(file_bytes, 1)
+    img = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB) / 255.0
+    img_resize = cv2.resize(img, dsize=(200, 200), interpolation=cv2.COLOR_BGR2RGB)
+    img_resize = np.resize(img_resize, (1,200,200,3))
+    loaded_imgs.append(img)
+
+    result = predict_image(img_resize, ENDPOINT)
     results.append(result)
-    loaded_imgs.append(load_image(image_file))
     captions.append("Prediction: {}".format(result))
 
 st.image(loaded_imgs, width=250, caption=captions)
